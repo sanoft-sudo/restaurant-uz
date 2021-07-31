ace3d12a27c32fe2af518cb8f07bcc09496880e7
Sanjar Juraev <72341995+sanoft-sudo@users.noreply.github.com>
2021-07-31 16:31:47 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import TopNavbar from "./components/TopNavbar";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <TopNavbar />
+    </div>
+  );
+}
---
+++ src/components/TopNavbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledTopNav = styled.div`
+  width: 100%;
+  background: rgba(0, 0, 0, 0.1);
+
+  display: flex;
+  justify-content: space-between;
+
+  .top-navDivs {
+    display: flex;
+    align-items: center;
+    font-size: 13px;
+    color: #d4d1cb;
+  }
+  a,
+  span {
+    text-decoration: none;
+    color: #d4d1cb;
+    font-size: 13px;
+    margin-left: 5px;
+    display: inline-block;
+  }
+`;
+
+const TopNavbar = () => {
+  return (
+    <StyledTopNav>
+      <div className="top-navDivs">
+        <i className="fas fa-phone-alt"></i>
+        <a className="topAClass" href="tel:+998977088389">
+          +998977088389
+        </a>
+      </div>
+      <div className="top-navDivs">
+        <i className="fas fa-paper-plane"></i>
+        <a className="mailto" href="mailto:sajar.juraev0409@gmail.com">
+          your-restaurant@test.com
+        </a>
+      </div>
+      <div className="top-navDivs">
+        <i class="far fa-clock"></i>
+        <span>Open hours: Monday - Sunday 7:00 - 24:00</span>
+      </div>
+    </StyledTopNav>
+  );
+};
+export default TopNavbar;
